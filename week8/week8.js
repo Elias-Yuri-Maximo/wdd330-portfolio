434e9cccb54f682b698e61bca1490df8890999f6
Elias Maximo <eliasmaximus@gmail.com>
2022-02-26 14:45:21 -0300

    FINISH

--- week8/week8.js
+++ week8/week8.js
@@ -67,3 +67,85 @@ function drawImageToCanvas() {
     var context = canvas.getContext("2d");
     var image = document.getElementById("myImageElem");
 }
+
+var button = document.getElementById("saveButton");
+button.addEventListener("click", saveDrawing, false);
+
+
+window.addEventListener("load", drawImageToCanvas, false);
+
+function drawImageToCanvas() {
+    var canvas = document.getElementById("demo6");
+    var context = canvas.getContext("2d");
+    var image = document.getElementById("myImageElem");
+}
+
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+var image = document.getElementById("myImageElem");
+// draw the image at x=0 and y=0 on the canvas
+context.drawImage(image, 68, 68);
+var imageData = context.getImageData(0, 0, 1, 1);
+var pixelData = imageData.data;
+console.log(pixelData.length);
+
+function manipulateImage() {
+    var canvas = document.getElementById("demo7");
+    var context = canvas.getContext("2d");
+    var image = document.getElementById("secondImage");
+    context.drawImage(image, 60, 60);
+
+    var imageData = context.getImageData(0, 0, 200, 200);
+    
+    for (var i = 0; i < imageData.data.length; i += 4) {
+    var red = imageData.data[i];
+    var green = imageData.data[i + 1];
+    var blue = imageData.data[i + 2];
+        
+    var grayscale = red * 0.3 + green * 0.59 + blue * 0.11;
+        
+    imageData.data[i] = grayscale;
+    imageData.data[i + 1] = grayscale;
+    imageData.data[i + 2] = grayscale;
+    } 
+    context.putImageData(imageData, 0, 0);
+}
+
+
+
+
+function draw(video, context, canvas) { 
+    if (video.paused || video.ended) return false;
+    
+    drawOneFrame(video, context, canvas);
+    
+    // Start over! 
+    setTimeout(function(){ draw(video, context, canvas); }, 0); 
+}
+
+function drawOneFrame(video, context, canvas){
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    try {
+    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    var pixelData = imageData.data;
+    for (var i = 0; i < pixelData.length; i += 4) {
+        var red = pixelData[i];
+        var green = pixelData[i + 1];
+        var blue = pixelData[i + 2];
+        var grayscale = red * 0.3 + green * 0.59 + blue * 0.11;
+        pixelData[i] = grayscale;
+        pixelData[i + 1] = grayscale;
+        pixelData[i + 2] = grayscale;
+    }
+    
+    imageData.data = pixelData;
+    context.putImageData(imageData, 0, 0);
+    } catch (err) {
+    // error handling code will go here
+    }
+}
+
+///SVG
+
+
